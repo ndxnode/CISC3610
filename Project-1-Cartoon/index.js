b790765ca87749317a4f6c8c0b7010933d9c3203
Andy Wu <wuandy6418@gmail.com>
2024-02-21 23:58:58 -0500

    almost done with project 1

--- Project-1-Cartoon/index.js
+++ Project-1-Cartoon/index.js
@@ -5,6 +5,9 @@ const ctx = myCanvas.getContext("2d");
 ctx.fillStyle = "#d3d3d3";
 ctx.fillRect(0,0,800,500);
 
+// title 
+
+
 //sun
 ctx.beginPath();
 ctx.arc(0, 0, 125, 0, 2 * Math.PI);
@@ -12,9 +15,41 @@ ctx.fillStyle = 'yellow';
 ctx.fill();
 
 
+//ground
+ctx.scale(1, -1); // Flip the y-axis
+ctx.translate(0, -500);
+ctx.fillStyle = "grey";
+ctx.fillRect(0,0,800,25);
 
-//clouds
-//house
+groundLevel = 25
 
 //grass
-//ground
+
+ctx.beginPath();
+for (let i = 0; i < 400; i+=25 ) { // i = grass width
+    ctx.moveTo(25 + i, 25 + groundLevel);  //top
+    ctx.lineTo(50 + i, groundLevel); // Right base corner
+    ctx.lineTo(0 + i, groundLevel); // Left base corner
+    ctx.closePath();
+    ctx.fillStyle = 'green';
+    ctx.fill();
+}
+
+
+//house
+ctx.beginPath();
+ctx.fillStyle = "brown";
+ctx.fillRect(425,groundLevel,300,250);
+
+//house triangle
+ctx.beginPath();
+ctx.moveTo(425+150,400);  //top
+ctx.lineTo(725,275); // Right base corner
+ctx.lineTo(425,275); // Left base corner
+ctx.closePath();
+ctx.fillStyle = 'black';
+ctx.fill();
+
+//house door
+ctx.beginPath();
+ctx.fillStyle()
